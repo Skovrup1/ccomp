15c5d0f0b13055c7588381b2611c643ce76073a7
skovrup1 <40407100+Skovrup1@users.noreply.github.com>
2025-02-24 15:42:46 +0100

    rewrite scanner

--- src/main.c
+++ src/main.c
@@ -1,83 +1,45 @@
-#include <stdint.h>
+#include "cc_common.h"
+#include "cc_scanner.h"
+
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 
-#include "alloc.h"
-#include "cc_string.h"
-#include "lexer.h"
-
-#define MAX_MEMORY 1000
-
-String open_file(char const *path);
-
-// caller owns the String
-String open_file(char const *path) {
-    // open file for reading
-    FILE *file = fopen(path, "r");
-
-    assert(file && "fopen in open_file failed");
-
-    // seek end
-    fseek(file, 0, SEEK_END);
-
-    // get file size
-    uint32_t size = ftell(file);
-
-    // seek back to start
-    fseek(file, 0, 0);
-
-    String buf = string_alloc(size);
-
-    // read to end
-    uint32_t ret = fread(buf.data, sizeof(*buf.data), buf.size, file);
-    if (ret != buf.size) {
-        fprintf(stderr, "fread() failed: %u\n", ret);
-        exit(EXIT_FAILURE);
+static char *open_file(char const *path) {
+    FILE *file = fopen(path, "rb");
+    if (file == NULL) {
+        fprintf(stderr, "failed to open file \"%s\".\n", path);
+        exit(74);
     }
+
+    fseek(file, 0, SEEK_END);
+    usize file_size = ftell(file);
+    rewind(file);
+
+    char *buffer = malloc(file_size + 1);
+    if (buffer == NULL) {
+        fprintf(stderr, "out of memory to read \"%s\".\n", path);
+        exit(74);
+    }
+
+    usize bytes_read = fread(buffer, sizeof(char), file_size, file);
+    if (bytes_read < file_size) {
+        fprintf(stderr, "could not read file \"%s\".\n", path);
+    }
+
+    buffer[bytes_read] = '\0';
 
     fclose(file);
-
-    return buf;
+    return buffer;
 }
 
-int main(int argc, char **argv) {
-    char const *path = "tests/return_val.c";
-
+i32 main(i32 argc, char *argv[]) {
     if (argc == 2) {
-        path = argv[1];
-        printf("running on %s\n", path);
+        char *buffer = open_file(argv[1]);
+        lexical_scan(buffer);
+    } else {
+        fprintf(stderr, "invalid arguments\n");
+        return 1;
     }
-
-    uint32_t mem_size = MAX_MEMORY;
-    unsigned char *mem_buf = malloc(mem_size);
-    mem_init(mem_buf, mem_size);
-
-    uint32_t temp_size = MAX_MEMORY;
-    unsigned char *temp_buf = malloc(temp_size);
-    temp_init(temp_buf, temp_size);
-
-    String source = open_file(path);
-
-    for (size_t i = 0; i < source.size; i++) {
-        printf("%c", source.data[i]);
-    }
-    printf("\n");
-
-    Lexer l = {
-        .source = source,
-    };
-
-    TokenArray tokens = lexer_scan(&l);
-
-    printf("tokens:\n");
-    for (size_t i = 0; i < tokens.size; i++) {
-        printf("    %s\n", token_type_to_string(tokens.data[i].type));
-    }
-    printf("\n");
-
-    free(mem_buf);
-    free(temp_buf);
 
     return 0;
 }
